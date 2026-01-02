51b7f24579ecdf164293d6ba3ff137e22d6dfe6d
Mees Molenaar <mees.molenaar@bettyblocks.com>
2026-01-02 16:39:10 +0100

    chore: now add the create and update dependency

--- examples/app-test-component/src/lib.rs
+++ examples/app-test-component/src/lib.rs
@@ -1,10 +1,15 @@
 wit_bindgen::generate!({ generate_all });
 
+use crate::betty_blocks::create::create::create;
+use crate::betty_blocks::update::update::update;
 use crate::exports::betty_blocks::action::action;
 struct Component;
 
 impl action::Guest for Component {
-    fn run() -> Result<String, String>{
+    fn run() -> Result<String, String> {
+        let _ = create();
+        let _ = update();
+
         Ok("Ran action".to_string())
     }
 }
